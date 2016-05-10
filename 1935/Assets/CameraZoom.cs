71338a6113c2653ce094b0b579abf7bb50fedbb2
Joshua Jackson <washingtonfaulconer@gmail.com>
2016-05-10 19:42:21 +0100

    Initial Commit

---
+++ 1935/Assets/CameraZoom.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraZoom : MonoBehaviour 
+{
+
+    void LateUpdate()
+    {
+        float scrolling = Input.GetAxis("Mouse ScrollWheel");
+
+        float orthSize = Camera.main.orthographicSize;
+
+        if (scrolling > 0f && Camera.main.orthographicSize >= 230 && Camera.main.orthographicSize <= 730)
+        {
+            orthSize -= 30;
+        }
+        else if (scrolling < 0f && Camera.main.orthographicSize >= 230 && Camera.main.orthographicSize <= 730)
+        {
+            orthSize += 30;
+        }
+
+        Camera.main.orthographicSize = Mathf.Clamp(orthSize, 230, 730);
+    }
+}
