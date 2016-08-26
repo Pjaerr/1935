b33c4552a604572ea2fb99589f115314fe652ced
Joshua Jackson <washingtonfaulconer@gmail.com>
2016-08-26 11:33:52 +0100

    Negligble Commit [Pre-Alpha v1] [Snapshot 0.2]
    Commented a lot of code to make it more maintainable and readable.

--- 1935/Assets/Scripts/infantryUnit.cs
+++ 1935/Assets/Scripts/infantryUnit.cs
@@ -24,24 +24,6 @@ public class infantryUnit : MonoBehaviour
         }
     }
 
-    void OnMouseDown()
-    {
-        actionMenu.SetActive(true); // Open action menu.
-
-        Debug.Log("Unit clicked!");
-    }
-
-    public void closeActionMenu()
-    {
-        actionMenu.SetActive(false); // Close action menu.
-    }
-
-    public void moveUnitClicked() //Method is called when the 'Move Unit' button is pressed.
-    {
-        placePinActive = true;
-        actionMenu.SetActive(false);
-    }
-    
     void placePin() //Method is called in the Update() function if placePinActive is true.
     {
         movementPin.SetActive(true); //Activates the movementPin game object so it can be shown on screen.
@@ -64,4 +46,37 @@ public class infantryUnit : MonoBehaviour
         //Moves the unit towards the movement pin that was placed down at the current global movementSpeed.
         GetComponent<Transform>().transform.position = Vector2.MoveTowards(currentPos, movementPinPos, movementSpeed * Time.deltaTime);
     }
+
+    void OnMouseDown()
+    {
+        actionMenu.SetActive(true); // Open action menu.
+
+        Debug.Log("Unit clicked!");
+    }
+
+    public void closeActionMenu()
+    {
+        actionMenu.SetActive(false); // Close action menu.
+    }
+
+    public void moveUnitClicked() //Method is called when the 'Move Unit' button is pressed.
+    {
+        placePinActive = true;
+        actionMenu.SetActive(false);
+    }
+    
+
+    /*Unit Movement Description
+    Methods: OnMousedown(), closeActionMenu(), Update(), placePin(), moveUnit()
+    placePinActive is set to true when the moveUnitClicked() method is called, causing the Update() function to start calling placePin() which will
+    show the Movement Pin sprite and make it follow the mouse cursor, this function will also check if the left mouse is clicked and, if so, will instantiate
+    a copy of the Movement Pin and place it at the position the mouse cursor was at when clicked, it will then set the placePinActive to false so that the Update() function stops calling
+    the placePin() function and the Movement Pin will dissapear and stop following the cursor. It will then set the moveUnitActive bool to true so that the Update() function can start calling
+    the moveUnit() function which will get the units current position and move it towards the position of the copy of the movement pin that was placed via the placePin() function.
+    TODO;
+    .Make the copy of the movement pin delete itself when the unit reaches its position.
+    .Add several checks (when networking is in place) to see whether the pin is being placed in the sea or in someone elses territory before actually placing the pin, 
+    asking the user for confirmation first.
+    .Make sure the user can only place one pin per unit at a time. If they place a new pin, remove the other pin.
+    */
 }
