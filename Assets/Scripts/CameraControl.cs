1da1111f9063351b1d8f00a5c1fffbb18399daf5
Joshua Jackson <washingtonfaulconer@gmail.com>
2017-04-26 15:30:59 +0100

    Setup GameManager and nation select.
    GameManager.cs is now setup and is a singleton. Nation enumerator can be
    chosen from and assigned to thisNation. That Nation thisNation can be
    accessed via other scripts, allowing stuff to occur dependant upon which
    nation is currently chosen. Temporary nation select drop down menu
    implemented, even though enums can be changed from the inspector.
    
    Also messed with pixelating the whole map. I think staying with the
    original is the best option however.

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+ {
+	Camera thisCamera;
+	void Start () 
+	{
+		thisCamera = GetComponent<Camera>();
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour 
+{
+	/*Nation Select UI */
+	[SerializeField] GameObject NationSelectPanel;
+	[SerializeField] private Dropdown NationSelectDropdown;
+
+	/*Nation Enumerator[s]*/
+	public enum Nation{Austria, Belgium, Denmark, Finland, France, Germany, Netherlands, Norway, Poland, Portugal, Spain, Sweden, Switzerland, UK};
+	public Nation thisNation;	//The Nation that this client is playing as.
+
+	public static GameManager singleton = null;	//Singleton instance.
+
+	void InitializeSingleton()
+	{
+		//Check if an instance of NationManager already exists.
+		if (singleton == null)
+		{
+			//If not, make this that instance.
+			singleton = this;
+		}
+		//If an instance already exists and it isn't this.
+		else if (singleton != this)
+		{
+			//Destroy this.
+			Destroy(gameObject);
+		}
+
+		DontDestroyOnLoad(gameObject);
+	}
+	
+	/*Will set the thisNation to the value chosen within the function. Currently uses a dropdown menu and will select the nation chosen.*/
+	public void NationSelect()
+	{
+		switch (NationSelectDropdown.value)
+		{
+			case 0:
+				thisNation = Nation.Austria;
+				break;
+			case 1:
+				thisNation = Nation.Belgium;
+				break;
+			case 2:
+				thisNation = Nation.Denmark;
+				break;
+			case 3:
+				thisNation = Nation.Finland;
+				break;
+			case 4:
+				thisNation = Nation.France;
+				break;
+			case 5:
+				thisNation = Nation.Germany;
+				break;
+			case 6:
+				thisNation = Nation.Netherlands;
+				break;
+			case 7:
+				thisNation = Nation.Norway;
+				break;
+			case 8:
+				thisNation = Nation.Poland;
+				break;
+			case 9:
+				thisNation = Nation.Portugal;
+				break;
+			case 10:
+				thisNation = Nation.Spain;
+				break;
+			case 11:
+				thisNation = Nation.Sweden;
+				break;
+			case 12:
+				thisNation = Nation.Switzerland;
+				break;
+			case 13:
+				thisNation = Nation.UK;
+				break;
+			default:
+				Debug.Log("Unknown Nation. Setting thisNation to Austria");
+				thisNation = Nation.Austria;
+				break;
+		}
+		Debug.Log("Nation set to " + thisNation + " @ NationManager::NationSelect()");
+		NationSelectPanel.SetActive(false);
+	}
+
+	void Awake()
+	{
+		InitializeSingleton();
+	}
+	void Start () 
+	{
+		NationSelectPanel.SetActive(true);
+	}
+}
