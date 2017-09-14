aed1881dc41c901aa8924edd5426a3640d8c9f26
Joshua Jackson <washingtonfaulconer@gmail.com>
2017-09-14 15:33:00 +0100

    Misc.

--- Assets/Scripts/Province.cs
+++ Assets/Scripts/Province.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+public class ProvinceValues : ValueContainer
+{
+	private int[] values = new int[] {0, 0, 0, 0, 0, 0};
+	new public enum type{Economy, Food, Iron, Coal, Happiness, Population};
+}
+
 public class Province
 {
 	public string name;
@@ -11,7 +18,7 @@ public class Province
 	/*These are the data values. value[0] is the actual value and value[1] is the amount
 	by which the first value will be changed every so often where:
 	0 = Economy, 1 = Food, 2 = Iron, 3 = Coal, 4 = Happiness, 5 = Population*/
-	public int[] values = new int[] {0, 0, 0, 0, 0, 0};
+	public ProvinceValues values = new ProvinceValues();
 	public int[] modifiers = new int[] {0, 0, 0, 0, 0, 0};
 
 	public List<Building> inactiveBuildings = new List<Building>();
--- Assets/Scripts/ValueObjects.cs
+++ Assets/Scripts/ValueObjects.cs
@@ -4,39 +4,22 @@ using UnityEngine;
 
 public class ValueContainer
 {
-	/*These are the data values. value[0] is the actual value and value[1] is the amount
-	by which the first value will be changed every so often where:
-	0 = Economy, 1 = Food, 2 = Iron, 3 = Coal, 4 = Happiness, 5 = Population*/
-	private int[] values;
-	public enum type{};
-
-	public int[] getAll()
-	{
-		return values;
-	}
-
-	public int get(type value)
-	{
-		return values[(int)value];
-	}
-
-	public void set(type value, int newValue)
-	{
-		values[(int)value] = newValue;
-	}
-}
-//UNFINISHED
-public class ProvinceValues : ValueContainer
-{
-	new enum type{Economy, Food, Iron, Coal, Happiness, Population};
+	/* 0 = Economy, 1 = Food, 2 = Iron, 3 = Coal, 4 = Happiness, 5 = Population*/
+	private IDictionary<string, float> values = new Dictionary<string, float>();
 	
-
-	public ProvinceValues()
+	public void addValue(string name, float value)
 	{
-		values = new int[] {0, 0, 0, 0, 0, 0};
+		values.Add(name, value);
+	}
+
+	public float? getValue(string key)
+	{
+		if (!values.ContainsKey(key))
+		{
+			return null;
+		}
+		
+		return values[key];
 	}
 }
-
-
-
 
